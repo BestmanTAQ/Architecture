875bee649aadfd2deb90d31b14c409ab89b44008
BestmanTAQ <bestman777taq@gmail.com>
2022-10-25 15:55:28 +0300

    Input Service

---
+++ Assets/CodeBase/Services/Input/InputService.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Services.Input
+{
+    public abstract class InputService : IInputService
+    {
+        protected const string Horizontal = "Horizontal";
+        protected const string Vertical = "Vertical";
+        private const string Button = "Fire";
+
+        public abstract Vector2 Axis { get; }
+
+        public bool IsAttackButtonDown() => SimpleInput.GetButtonDown(Button);
+
+        protected static Vector2 SimpleInputAxis()
+        {
+            return new Vector2(SimpleInput.GetAxis(Horizontal), SimpleInput.GetAxis(Vertical));
+        }
+    }
+}
